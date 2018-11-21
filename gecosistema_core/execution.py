087fbf0c359eb7ce90ecb3b7d2f30151c9ef231c
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2018-11-21 10:30:39 +0100

    version 0.0.137

--- gecosistema_core/execution.py
+++ gecosistema_core/execution.py
@@ -75,6 +75,9 @@ def Exec(command, env={}, precond=[], postcond=[], remove=[], skipIfExists=False
             try:
                 #subprocess.call(args, shell=False)
                 outdata = subprocess.check_output(command, stderr=subprocess.STDOUT).decode()
+                print("outdata:")
+                print(outdata)
+                print("-----")
                 if "[" in outdata or "{" in outdata:
                     outdata = json.loads(outdata)
 
